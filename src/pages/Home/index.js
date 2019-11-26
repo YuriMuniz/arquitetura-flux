ccf373fdef8032b3f834b01666602a1cfc907fe5
YMG <y.munizgonzalez@gmail.com>
2019-11-26 19:10:04 -0300

    estilização home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,96 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {MdAddShoppingCart} from 'react-icons/md';
+
+import {ProductList} from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/kit-2-pares-tenis-kappa-impact-masculino/04/D24-1981-304/D24-1981-304_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong> Tênis muito legal</strong>
+                <span> 129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }

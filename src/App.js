0b5deef5e7a0b4c015c56cf96ba822a62a3bada5
YMG <y.munizgonzalez@gmail.com>
2019-11-26 18:42:56 -0300

    criação header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import {BrowserRouter} from 'react-router-dom';
 
 import GlobalStyles from './styles/global';
 
+import Header from './components/Header';
+
 import Routes from './routes';
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes />
             <GlobalStyles />
         </BrowserRouter>

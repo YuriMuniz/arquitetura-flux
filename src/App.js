7ee11c243ed7e2e35dd17ab533066d4a2c08c111
YMG <y.munizgonzalez@gmail.com>
2019-12-25 00:10:36 -0300

    Configuração redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import GlobalStyles from './styles/global';
 
@@ -7,13 +8,17 @@ import Header from './components/Header';
 
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes />
-            <GlobalStyles />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Header />
+                <Routes />
+                <GlobalStyles />
+            </BrowserRouter>
+        </Provider>
     );
 }
 
